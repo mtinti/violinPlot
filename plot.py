ac2148411d95b490814c0444d4fde2abddb011d4
mtinti <michele.tinti@gmail.com>
2017-10-17 16:14:46 +0100

    add columns selection, log option, drop na\inf

--- plot.py
+++ plot.py
@@ -5,15 +5,18 @@ import pandas as pd
 import seaborn as sns
 sns.set_style("whitegrid")
 import matplotlib.pyplot as plt
-
+import numpy as np
 #facilitate violin plot of a dataframe using
 #seaborn
 class ViolinPlot():
     def __init__(self, df = pd.DataFrame()):
         self.df = df
     
-    def plot(self, cols, ax):
+    def plot(self, cols=None, ax='',log=False):
         #rearrange columns (vertical stack)
+        if cols == None:
+            cols=self.df.columns
+            print cols
         selection =  self.df[cols]
         temp_1 = [selection[n].to_frame() for n in cols]
         temp_2 =[]
@@ -22,6 +25,11 @@ class ViolinPlot():
             temp_df.columns = ['value', 'col']
             temp_2.append(temp_df)
         temp_2 = pd.concat(temp_2,ignore_index=True)
+        print temp_2.head()
+        if log ==True:
+            temp_2['value']=np.log10(temp_2['value'])
+            temp_2 = temp_2.replace([np.inf, -np.inf], np.nan)
+            temp_2.dropna(inplace=True)
         ax=sns.violinplot(x='col', y='value', data=temp_2, ax=ax)
         plt.xticks(rotation=40,ha='right')
         return ax
